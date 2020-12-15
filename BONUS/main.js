85b168a746f687bac1212a416a41c35c24509fcc
Alessandro <internicola.alessandro@gmail.com>
2020-12-15 17:01:26 +0100

    Bonus 1 completato
    Aggiunta una funzione setinteval con mounted.

--- BONUS/main.js
+++ BONUS/main.js
@@ -5,13 +5,19 @@ let app = new Vue({
   data: {
     counter: 0,
     images: [
-      "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg",
-      "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg",
-      "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
-      "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"
-    ]
+      "img/falcon.jpg",
+      "img/jedi-starfighter.webp",
+      "img/tie.webp",
+      "img/y-wing.jpg",
+      "img/x-wing.webp"
+  ]
   },
 
+  mounted: function nextImg() {
+    this.$nextTick(function () {
+      setInterval(this.next, 2000);
+    })
+  },
 
   methods: {
 
